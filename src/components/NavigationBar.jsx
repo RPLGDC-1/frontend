dcb67da169e41a64f425ca34c191404bbe8d6802
M Isa Al Anshori <isabungsu@gmail.com>
2023-10-03 20:29:04 +0700

    Footer & Navigation Bar: Disable Dummy Links

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -35,7 +35,7 @@ export default function NavigationBar() {
         <div className="flex flex-col top-0 sticky z-50 justify-center items-center bg-[#1e1e1e] text-[#cdcdcd]">
             <Link className='sm:hidden p-4 h-20' to="/"><img className="h-full" src={DBLogo} alt="Dunia Belanja"></img></Link>
             <div className="w-full flex flex-row justify-evenly sm:text-xl items-stretch ">
-                <NavLink className="navlink w-full flex items-center justify-center bg-black bg-opacity-0 hover:bg-opacity-50" to="/404">ABOUT US</NavLink>
+                <Link className="navlink w-full flex items-center justify-center bg-black bg-opacity-0 hover:bg-opacity-50">ABOUT US</Link>
                 <NavLink className="navlink w-full flex items-center justify-center bg-black bg-opacity-0 hover:bg-opacity-50" to="/products">PRODUCT</NavLink>
                 <Link className='hidden sm:block p-4 w-full h-20' to="/"><img className="w-full h-full" src={DBLogo} alt="Dunia Belanja"></img></Link>
                 {userProfile 
